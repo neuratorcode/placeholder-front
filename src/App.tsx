45b054c0272de722269a006f0fc314ee85e8ca52
Yann <yanncotineau@gmail.com>
2025-03-09 21:03:55 +0100

    front placeholder

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/neurator.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://www.youtube.com/watch?v=8GW6sLrK40k" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Work in Progress</h1>
+      <p>Let me cook 🍳🔥</p>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
